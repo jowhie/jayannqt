d59461300ffdfc201f6843376b981dba7e20b9fc
Joey <joeylabisto@gmail.com>
2026-02-14 15:18:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "To MY Dearest",
+    valentineName: "To My Dearest",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
